4a751dbe900de75dda54115365e75bfa448ed9c1
CVSCharan <charan.cvs@gmail.com>
2022-08-12 02:37:06 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [backgroundColor, setbackgroundColor] = useState("lightgrey");
+  let dummy = [
+    "green",
+    "black",
+    "pink",
+    "royalblue",
+    "blue",
+    "red",
+    "yellow",
+    "skyblue",
+    "darkgreen",
+    "lightgreen",
+    "lightred",
+  ];
+
+  const onClickButton = () => {
+    const rand = Math.ceil(Math.random() * 10);
+    setbackgroundColor(dummy[rand]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container main-container">
+      <div class="row">
+        <div class="col-12 col-md-6 d-flex flex-column justify-content-center">
+          <div class="text-center p-4">
+            <h1 class="instruction">Click the below button to change color</h1>
+            <button id="button" class="button" onClick={onClickButton}>
+              Click Me
+            </button>
+          </div>
+        </div>
+        <div
+          id="bgContainer"
+          class="col-12 col-md-6 bg-container"
+          style={{ backgroundColor: backgroundColor }}
+        ></div>
+      </div>
     </div>
   );
 }
